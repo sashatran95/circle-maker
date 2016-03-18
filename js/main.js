a9b64e5444540e9e59b5c36f41105117fdda0c8c
Sashtastic <ht.sasha.tran@gmail.om>
2016-03-18 16:59:38 -0400

    Circles everywhere

--- js/main.js
+++ js/main.js
@@ -1,34 +1,11 @@
-$('#click-me').on('click', function () {
-  alert('Hello, World!');
+var $body = $('body');
+var $html = $('html');
+
+$html.on('keydown', function () {
+  var $newBall = $('<div>');
+
+  $newBall.addClass('ball');
+  $body.append($newBall);
+  $newBall.css('left', Math.random() * 1000);
+  $newBall.css('top', Math.random() * 1000);
 });
-
-$('ul').on('click', function (e) {
-  $(this).toggleClass('is-clicked');
-});
-
-
-//var $ball = $('.ball');
-
-//$('#btn-right').on('click', function () {
-//  var newLeft = $ball.offset().left + 10;
-//  $ball.css('left', newLeft);
-
-//});
-
-//$('#btn-left').on('click', function () {
-//  var newRight = $ball.offset().left - 10;
-//  $ball.css('left', newRight);
-
-//});
-
-//$('#btn-up').on('click', function () {
-//  var newDown = $ball.offset().top - 10;
-//  $ball.css('top', newDown);
-
-//});
-
-//$('#btn-down').on('click', function () {
-//  var newUp = $ball.offset().top + 10;
-//  $ball.css('top', newUp);
-
-//});
